ff17d5fe4c22ec458cc40e4f5eb78ba72b1264b9
Rushyang Darji <rushyangdarji@gmail.com>
2023-08-10 11:46:34 +1000

    Few theme changes

--- nvim/LuaMigration/lua/themes.lua
+++ nvim/LuaMigration/lua/themes.lua
@@ -1,5 +1,2 @@
--- require("themes_config.colorscheme")
-vim.o.termguicolors = true
-require("themes_config.catppuccin")
+require("themes_config.colorscheme")
 require("themes_config.themes_overrides")
--- require("themes_config.testoverride")
