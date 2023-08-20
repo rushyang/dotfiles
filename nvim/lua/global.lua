3893fc681f77d3ed4d336bb4e62cd9817dabcbf9
Rushyang Darji <rushyangdarji@gmail.com>
2023-08-20 12:39:46 +1000

    Restructured all keymaps for Telescope description search friendly

--- nvim/lua/global.lua
+++ nvim/lua/global.lua
@@ -1,3 +1,8 @@
+-- Global Opts
+opts = { noremap = true, silent = true }                                  -- opts: options silent
+optns = { noremap = true }                                                -- optns: options non-silent
+keymap = vim.api.nvim_set_keymap                                          -- Shortern function's name
+vkeymap = vim.keymap.set
 -- Get_git_tag function
 function get_git_tag()
     local git_tag = vim.fn.system('git tag --points-at HEAD 2>/dev/null | xargs'):gsub("\n", "")
@@ -7,4 +12,16 @@ end
 -- get_absolute_path function
 function get_absolute_path()
     return vim.fn.expand('%:p')
+end
+
+-- Extend mapping options (For Displaying Description in Telescope keymaps)
+function eopts(options)
+   local eopts= {}
+   for k, v in pairs(opts) do
+      eopts[k] = v
+   end
+   for k, v in pairs(options) do
+      eopts[k] = v
+   end
+   return eopts
 end
--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,8 +1,4 @@
 -- Keymaps
-
-local opts = { noremap = true, silent = true }                                  -- opts: options silent
-local optns = { noremap = true }                                                -- optns: options non-silent
-local keymap = vim.api.nvim_set_keymap                                          -- Shortern function's name
 local g = vim.g
 
 -- Remap space as the leader key
@@ -12,90 +8,90 @@ g.maplocalleader = " "
 
 -- Normal Mode --
 -- (N) Better Window Navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-h>", "<C-w>h", eopts({desc = "Go to Left Buffer"}))
+keymap("n", "<C-j>", "<C-w>j", eopts({desc = "Go to Below Buffer "}))
+keymap("n", "<C-k>", "<C-w>k", eopts({desc = "Go to Upper Buffer"}))
+keymap("n", "<C-l>", "<C-w>l", eopts({desc = "Go to Right Buffer"}))
 
 -- (N) Search Settings
-keymap("n", "ss", ":noh<CR>", optns)                                          -- clear search highlight
+keymap("n", "ss", ":noh<CR>", eopts({desc = "Clear Search Highlight"}))
 
 -- (N) Window Splits/Rotations/Maximize/Resize
-keymap("n", "<Leader>s", [[<Cmd>horizontal split<CR>]], opts)                 -- Horizontal Split of Same Buffer
-keymap("n", "<M-s>", ":horizontal split | :Files ~<CR>", opts)                -- Horizontal Split buffer + Fuzzy Search
-keymap("n", "<Leader>v", [[<Cmd>vertical split<CR>]], opts)                   -- Vertical Split of Same Buffer
-keymap("n", "<M-1>", "<C-w>K", opts)                                          -- Move Vertical split to Horizontal
-keymap("n", "<M-2>", "<C-w>H", opts)                                          -- Move Horizontal split to Vertical
-keymap("n", "<C-Space>r", "<C-w><C-r>", opts)                                 -- Interchange buffers - Horizontal takes precedence
-keymap("n", "<C-Space>_", ":wincmd |<CR>", opts)                              -- Vertical Maximize
-keymap("n", "<C-Space>|", ":wincmd _<CR>", opts)                              -- Horizontal Maximize
-keymap("n", '<C-Space>+', [[:wincmd _ | :wincmd |<CR>]], opts)                -- Horizontal Maximize
-keymap("n", "<C-Right>", [[<Cmd>:vertical resize +3<CR>]], opts)
-keymap("n", "<C-Left>", [[<Cmd>:vertical resize -3<CR>]], opts)
-keymap("n", "<C-Up>", [[<Cmd>:horizontal resize +3<CR>]], opts)
-keymap("n", "<C-Down>", [[<Cmd>:horizontal resize -3<CR>]], opts)
+keymap("n", "<Leader>s", [[<Cmd>horizontal split<CR>]], eopts({desc = "Horizontal Split of Same Buffer"}))
+keymap("n", "<M-s>", ":horizontal split | :Files ~<CR>", eopts({desc = "Horizontal Split Buffer and Fuzzy Search"}))
+keymap("n", "<Leader>v", [[<Cmd>vertical split<CR>]], eopts({desc = "Vertical Split of Same Buffer"}))
+keymap("n", "<M-1>", "<C-w>H", eopts({desc = "Move Horizontal split to Vertical"}))
+keymap("n", "<M-2>", "<C-w>K", eopts({desc = "Move Vertical split to Horizontal"}))
+keymap("n", "<C-Space>r", "<C-w><C-r>", eopts({desc = "Interchange Buffers"}))
+keymap("n", "<C-Space>_", ":wincmd |<CR>", eopts({desc = "Vertical Maximize"}))
+keymap("n", "<C-Space>|", ":wincmd _<CR>", eopts({desc = "Horizontal Maximize"}))
+keymap("n", '<C-Space>+', [[:wincmd _ | :wincmd |<CR>]], eopts({desc = "Maximize"}))
+keymap("n", "<C-Right>", [[<Cmd>:vertical resize +3<CR>]], eopts({desc = "Vertical Resize +3"}))
+keymap("n", "<C-Left>", [[<Cmd>:vertical resize -3<CR>]], eopts({desc = "Vertical Resize -3"}))
+keymap("n", "<C-Up>", [[<Cmd>:horizontal resize +3<CR>]], eopts({desc = "Horizontal Resize +3"}))
+keymap("n", "<C-Down>", [[<Cmd>:horizontal resize -3<CR>]], eopts({desc = "Horizontal Resize -3"}))
 
 -- (N) Quick Fuzzy Search / Split Modes
-keymap("n", "<Leader>f", [[<Cmd>Files ~<CR>]], opts)
-keymap("n", "<C-Space><C-f>", [[<C-U>vsp | <Cmd>Files ~<CR>]], opts)
-keymap("n", "<C-Space><C-f>", ":<C-U>vsp | :Files ~<CR>", opts)
-keymap("n", "<Leader>h", ":History<CR>", opts)
-keymap("n", "<C-Space><C-h>", ":<C-U>vsp | :History<CR>", opts)
-keymap("n", "<Leader>b", ":Buffers<CR>", opts)
-keymap("n", "<C-Space><C-b>", ":<C-U>vsp | :Telescope buffers<CR>", opts)
-keymap("n", "<Leader>r", ":Ranger<CR>", opts)
-keymap("n", "<C-Space><C-l>", ":<C-U>vsp | :Ranger<CR>", opts)
-keymap("n", "<Leader>g", ":Telescope git_files<CR>", opts)
-keymap("n", "<C-Space><C-g>", ":<C-U>vsp | :Telescope git_files<CR>", opts)
-keymap("n", "<Leader>t", '<Cmd>lua telescope_live_grep_in_git_project()<CR>', optns)
+keymap("n", "<Leader>f", [[<Cmd>Telescope find_files cwd=/home/apollo hidden=true<CR>]], eopts({desc = "Search [F]iles in $HOME"}))
+keymap("n", "<C-Space><C-f>", [[<Cmd>vertical split | :Telescope find_files cwd=/home/apollo hidden=true<CR>]], eopts({desc = "Vertical Split and Search [F]iles in $HOME"}))
+keymap('n', '<Leader>h', [[<Cmd>lua require('telescope.builtin').oldfiles()<CR>]], eopts({desc = "[H]istory - previously opened files"}))
+keymap("n", "<C-Space><C-h>", [[<Cmd>vertical split | lua require('telescope.builtin').oldfiles()<CR>]], eopts({desc = "Vertical Split and [H]istory Search"}))
+keymap("n", "<Leader>b", [[<Cmd>Telescope buffers<CR>]], eopts({desc = "Open [B]uffers"}))
+keymap("n", "<C-Space><C-b>", [[<Cmd>vertical split | :Telescope buffers<CR>]], eopts({desc = "Vertical Split and Open [B]uffers"}))
+keymap("n", "<Leader>r", ":Ranger<CR>", eopts({desc = "Open Ranger in current buffer"}))
+keymap("n", "<C-Space><C-l>", ":vertical split | :Ranger<CR>", eopts({desc = "Vertical Split and Open Ranger"}))
+-- keymap("n", "<Leader>g", ":GitFiles<CR>", eopts({desc = "Fuzzy search of files in current Git project"}))
+-- keymap("n", "<C-Space><C-g>", ":<C-U>vsp | :GitFiles<CR>", eopts({desc = "Vertical Split and fuzzy search of files in current Git project"}))
+keymap("n", "<Leader>t", '<Cmd>lua telescope_live_grep_in_git_project()<CR>', eopts({desc = "Live grep in Git Project"}))
+keymap("n", "<Leader>k", '<Cmd>Telescope keymaps <CR>', eopts({desc = "Open Telescope [k]eymaps"}))
 
 -- (N) New Tabs, Cycle through the tas and buffers
 keymap("n", ")", ":tabnext<CR>", optns)
 keymap("n", "(", ":tabprevious<CR>", optns)
 keymap("n", "<C-t>", ":tabnew<CR>", optns)
 keymap("n", "<M-t>", ":tabnew | Files ~<CR>", optns)
-keymap("n", "<C-Space>1", "1gt", opts)
-keymap("n", "<C-Space>2", "2gt", opts)
-keymap("n", "<C-Space>3", "3gt", opts)
-keymap("n", "<C-Space>4", "4gt", opts)
-keymap("n", "<C-Space>5", "5gt", opts)
-keymap("n", "<C-Space>6", "6gt", opts)
-keymap("n", "<C-Space>7", "7gt", opts)
-keymap("n", "<C-Space>8", "8gt", opts)
-keymap("n", "<C-Space>9", "9gt", opts)
-keymap("n", "}", ":bnext<CR>", optns)
-keymap("n", "{", ":bprevious<CR>", optns)
-keymap("n", "_", ":bdelete<CR>", optns)
+keymap("n", "<C-Space>1", "1gt", eopts({desc = "Go to Neovim Tab# 1"}))
+keymap("n", "<C-Space>2", "2gt", eopts({desc = "Go to Neovim Tab# 2"}))
+keymap("n", "<C-Space>3", "3gt", eopts({desc = "Go to Neovim Tab# 3"}))
+keymap("n", "<C-Space>4", "4gt", eopts({desc = "Go to Neovim Tab# 4"}))
+keymap("n", "<C-Space>5", "5gt", eopts({desc = "Go to Neovim Tab# 5"}))
+keymap("n", "<C-Space>6", "6gt", eopts({desc = "Go to Neovim Tab# 6"}))
+keymap("n", "<C-Space>7", "7gt", eopts({desc = "Go to Neovim Tab# 7"}))
+keymap("n", "<C-Space>8", "8gt", eopts({desc = "Go to Neovim Tab# 8"}))
+keymap("n", "<C-Space>9", "9gt", eopts({desc = "Go to Neovim Tab# 9"}))
+keymap("n", "}", ":bnext<CR>", eopts({desc = "Next Buffer"}))
+keymap("n", "{", ":bprevious<CR>", eopts({desc = "Previous Buffer"}))
+keymap("n", "_", ":bdelete<CR>", eopts({desc = "Delete Buffer"}))
 
 -- (N) Save and Exit
-keymap("n", "<Leader>k", "<C-w><C-q>", opts)
-keymap("n", "<C-q>", ":q<CR>", opts)
-keymap("n", "<Leader><C-q>", ":q<CR>", opts)
-keymap("n", "<C-w>", ":w<CR>", opts)
+keymap("n", "<C-q>", ":q<CR>", eopts({desc = "[Q]uit current buffer"}))
+keymap("n", "<Leader><C-q>", ":q<CR>", eopts({desc = "[Quit] current buffer"}))
+keymap("n", "<C-w>", ":w<CR>", eopts({desc = "[W]rite current buffer"}))
 
 -- (N) Diff mode / Fugitive / Diffview
 keymap("n", "<Leader>d", [[<Cmd>windo diffthis<CR>]], optns)
 keymap("n", "<C-Space><C-d>", [[<Cmd>windo diffoff<CR>]], optns)
-keymap("n", "<C-Space>G", ":Gvdiffsplit! ", optns)                                                    -- Fugitive vertical diff call - non-silent and without <Cmd> + <CR> for params
-keymap("n", "<C-Space><M-0>", ":0Gllog <CR>", optns)                                                  -- Fugitive - see commit history of just current file
-keymap("n", "<M-g>", ':lua GoldenSizeToggle()<CR> :DiffviewOpen <CR>', optns)                         -- First disable Golden_Size and then DiffviewOpen
-keymap("n", "<C-Space><M-g>", ':lua GoldenSizeToggle()<CR> :DiffviewClose ', optns)                   -- Enable Golden_Size and then DiffviewClose but wait for prompt
-keymap("n", "H", "[c", opts)
-keymap("n", "L", "]c", opts)
+keymap("n", "<C-Space>G", ":Gvdiffsplit! ", optns)
+keymap("n", "<Leader>hd", ":Gvdiffsplit!<CR>", eopts({desc = "DiffSplit Visual of current file"}))
+keymap("n", "<C-Space><M-0>", ":0Gllog <CR>", optns)
+keymap("n", "<M-g>", ':lua focus_resize_0()<CR> :DiffviewOpen ', optns)
+keymap("n", "<C-Space><M-g>", ':lua focus_resize_1()<CR> :DiffviewClose ', optns)
+keymap("n", "H", "[c", eopts({desc = "Go to Previous hunk or Diffview Change"}))
+keymap("n", "L", "]c", eopts({desc = "Go to Next hunk or Diffview change"}))
 
 -- (N) Toggles
 keymap("n", "<C-Space><C-\\>", [[<Cmd>setlocal nonumber! | IndentBlanklineToggle <CR>]], optns)
 keymap("n", "<C-Space>ww", ":set wrap!<CR>", optns)
 keymap("n", "<C-Space><C-s>", ":set scrollbind! scrollbind?<CR>", optns)
 keymap('n', '<F2>', ':lua toggle_CoC()<CR>', { noremap = true })
-keymap('n', '<F3>', ':lua GoldenSizeToggle()<CR>', { noremap = true })
-keymap("n", "<F5>", ":set spell!<CR>", optns)
+keymap('n', '<F3>', ':FocusToggle<CR>', { noremap = true })
+keymap("n", "<F5>", ":set spell!<CR>", eopts({desc = "Spell Check Toggle"}))
 
 -- (N) Misc
-keymap("n", "Q", "q", optns)                                                                         -- Change Macro record to "Q"- nonsilent
-keymap("n", "<C-Space>s", ":source $MYVIMRC<CR>", optns)                                             -- Source init file
-keymap("n", "<Leader><C-w>", ":StripWhitespace<CR>", optns)                                          -- StripWhitespace
-keymap("n", "<C-Space>in", ":IndentBlanklineToggle<CR>", optns)                                      -- Toggle IndentBlankline
+keymap("n", "Q", "q", optns)
+-- keymap("n", "<C-Space>s", ":source $MYVIMRC<CR>", optns)
+keymap("n", "<Leader><C-w>", ":StripWhitespace<CR>", eopts({desc = "Strip Whitespaces"}))
+keymap("n", "<C-Space>in", ":IndentBlanklineToggle<CR>", eopts({desc = "IndentBlankline Toggle"}))
 
 -- Insert Mode
 -- (I) Change insert mode key:
@@ -103,10 +99,10 @@ keymap("i", "jj", "<Esc>`^", opts)
 keymap("i", "kk", "<Esc>`^", opts)
 
 -- Visual Mode
-keymap("v", "<F6>", "y<c-w>wp<c-w>pgv", opts)                                                        -- Text movement from one pane to another
-keymap("v", "<C-h>", "^", opts)                                                                      -- Start of the line
-keymap("v", "<C-l>", "$<Left>", opts)                                                                -- End of the line
-keymap("v", "y", "\"+y", opts)
+keymap("v", "<F6>", "y<c-w>wp<c-w>pgv", eopts({desc = "Text Movement from one pane to another"}))
+keymap("v", "<C-h>", "^", eopts({desc = "Move to start of line in visual mode"}))
+keymap("v", "<C-l>", "$<Left>", eopts({desc = "Move to end of line in visual mode"}))
+keymap("v", "y", "\"+y", eopts({desc = "Copy to System buffer"}))
 
 -- Terminal Mode
 -- keymap("t", "<Esc>", "<C-\\><C-n>:let g:isInsert=0<CR>", optns)
