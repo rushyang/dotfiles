d601036cd904af0806ae72a29a8888c5ac35a0e8
Rushyang Darji <rushyangdarji@gmail.com>
2023-11-22 18:48:02 +1100

    chore(Noice-notify): Disabling notify for messages

--- nvim/lua/plugins_config/NoiceConfig.lua
+++ nvim/lua/plugins_config/NoiceConfig.lua
@@ -29,16 +29,16 @@ require("noice").setup({
   -- Icons for completion item kinds (see defaults at noice.config.icons.kinds)
   kind_icons = {}, -- set to `false` to disable icons
   },
-  -- messages = {
-  --   -- NOTE: If you enable messages, then the cmdline is enabled automatically.
-  --   -- This is a current Neovim limitation.
-  --   enabled = true, -- enables the Noice messages UI
-  --   view = "notify", -- default view for messages
-  --   view_error = "notify", -- view for errors
-  --   view_warn = "notify", -- view for warnings
-  --   view_history = "messages", -- view for :messages
-  --   view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
-  -- },
+  messages = {
+    -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+    -- This is a current Neovim limitation.
+    enabled = false, -- enables the Noice messages UI
+    view = "notify", -- default view for messages
+    view_error = "notify", -- view for errors
+    view_warn = "notify", -- view for warnings
+    view_history = "messages", -- view for :messages
+    view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+  },
   -- Below is commented to check later. Its erroring out
   -- views = {
   --   cmdline_popup = {
