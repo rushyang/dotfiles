d13275d4350edfab7cf37876b5e3d623fefe5d69
Rushyang Darji <rushyangdarji@gmail.com>
2024-01-04 11:58:59 +1100

    feat(Copilot): Force not to use tab for accepting suggestion

--- nvim/lua/plugins_config/Copilot.lua
+++ nvim/lua/plugins_config/Copilot.lua
@@ -3,3 +3,8 @@ vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 vim.g.copilot_tab_fallback = ""
 vim.g.copilot_proxy = vim.env.HTTPS_PROXY
+
+-- vim.keymap.set('i', '<M-J>', 'copilot#Accept("\<CR>")', {
+--     expr = true,
+--     replace_keycodes = false
+-- })
