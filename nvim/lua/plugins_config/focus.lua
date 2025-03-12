90e0743838d5085b6ca0c22531cd99a990566658
Rushyang Darji <rushyangdarji@gmail.com>
2025-03-12 17:48:46 +1100

    fix(Focus+nvim-tree): Disable focus on nvim-tree buffer

--- nvim/lua/plugins_config/focus.lua
+++ nvim/lua/plugins_config/focus.lua
@@ -1,3 +1,33 @@
+-- Add exceptions for specific filetypes and buftypes
+local ignore_filetypes = { 'neo-tree','NvimTree' }
+local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
+local augroup =
+    vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+vim.api.nvim_create_autocmd('WinEnter', {
+    group = augroup,
+    callback = function(_)
+        if vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
+        then
+            vim.w.focus_disable = true
+        else
+            vim.w.focus_disable = false
+        end
+    end,
+    desc = 'Disable focus autoresize for BufType',
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    group = augroup,
+    callback = function(_)
+        if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+            vim.b.focus_disable = true
+        else
+            vim.b.focus_disable = false
+        end
+    end,
+    desc = 'Disable focus autoresize for FileType',
+})
+
 require("focus").setup({
     enable = true, -- Enable module
     commands = true, -- Create Focus commands
